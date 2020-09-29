80b15cafe19b77c2195082984b533d257b5f096c
SohyunKim <chloek9649@gmail.com>
2020-09-29 10:37:36 +0900

    sector class 구성요소 및 함수 추가

--- app/src/main/java/com/example/vision/Sector.java
+++ app/src/main/java/com/example/vision/Sector.java
@@ -1,12 +1,26 @@
 package com.example.vision;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Sector {
+
     private String id;
     private String name;
     private String type;
+    private Boolean dot;
+    private Boolean line;
+    private String escalator;
+    private Boolean stairs;
+    private Boolean pillar; //원기둥
+    private Boolean signBoard; //벽에 붙은 간판
+    private Boolean topBoard; //천장에 달린 간판
+    private Boolean subwayTracks; //탑승장
+    private Boolean exit;
+    private Boolean enterGate; //개찰구
+    private JSONArray GPS; // [위도, 경도]
+
 
     public Sector(){
 
@@ -17,6 +31,18 @@ public class Sector {
             this.id = job.getString("_id");
             this.name = job.getString("name");
             this.type = job.getString("type");
+            this.dot = job.getBoolean("dot");
+            this.line = job.getBoolean("line");
+            this.escalator = job.getString("escalator");
+            this.stairs = job.getBoolean("stairs");
+            this.pillar = job.getBoolean("pillar");
+            this.signBoard = job.getBoolean("signBoard");
+            this.topBoard = job.getBoolean("topBoard");
+            this.subwayTracks = job.getBoolean("subwayTracks");
+            this.exit = job.getBoolean("exit");
+            this.enterGate = job.getBoolean("enterGate");
+            this.GPS = job.getJSONArray("GPS");
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -25,20 +51,39 @@ public class Sector {
     public void setId(String id){
         this.id = id;
     }
-
     public void setName(String name) { this.name = name; }
-
     public void setType(String type){
         this.type = type;
     }
-
-    public String getId(){
-        return this.id;
+    public void setDot(Boolean dot){
+        this.dot=dot;
     }
+    public void setLine(Boolean line){
+        this.line = line;
+    }
+    public void setEscalator(String escalator) {this.escalator=escalator; }
+    public void setStairs(Boolean stairs){ this.stairs = stairs; }
+    public void setPillar(Boolean pillar){   this.pillar = pillar;    }
+    public void setSignBoard(Boolean signBoard) {this.signBoard = signBoard;}
+    public void setTopBoard(Boolean topBoard) {this.topBoard = topBoard;}
+    public void setSubwayTracks(Boolean subwayTracks) {this.subwayTracks=subwayTracks;}
+    public void setExit(Boolean exit) {this.exit = exit;}
+    public void setEnterGate(Boolean enterGate) { this.enterGate = enterGate;}
+    public void setGPS(JSONArray GPS) { this.GPS = GPS; }
 
+
+    public String getId(){ return this.id;}
     public String getName() { return this.name; }
-
-    public String getType(){
-        return this.type;
-    }
+    public String getType(){ return this.type; }
+    public Boolean getDot() { return this.dot; }
+    public Boolean getLine() {return this.line;}
+    public String getEscalator() { return this.escalator;}
+    public Boolean getStairs() { return this.stairs;}
+    public Boolean getPillar() { return this.pillar;}
+    public Boolean getSignBoard() { return this.signBoard;  }
+    public Boolean getTopBoard() { return this.topBoard; }
+    public Boolean getSubwayTracks() { return this.subwayTracks; }
+    public Boolean getExit() { return this.exit; }
+    public Boolean getEnterGate() { return this.enterGate; }
+    public JSONArray getGPS() { return this.GPS;}
 }
